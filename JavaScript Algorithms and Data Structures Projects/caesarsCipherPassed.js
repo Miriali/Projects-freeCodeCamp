8a8a0b6e0c6b63c30fd0d5433361694604441180
Miriali López <70168836+Miriali@users.noreply.github.com>
2020-10-26 12:50:18 -0400

    Update caesarsCipherPassed.js

--- JavaScript Algorithms and Data Structures Projects/caesarsCipherPassed.js
+++ JavaScript Algorithms and Data Structures Projects/caesarsCipherPassed.js
@@ -1,23 +1,23 @@
-	function rot13(str) { // LBH QVQ VG!
-		var asCode = [];
-		var decrypted = [];
-		  for (var i = 0; i < str.length; i++) {
-		    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
-		      if (str.charCodeAt(i) - 13 < 65) {
-		        asCode.push(str.charCodeAt(i) + 26 - 13);
-		      } else {
-		        asCode.push(str.charCodeAt(i) - 13);
-		      }
-		    } else {
-		      asCode.push(str.charCodeAt(i));
-		    }
-		  }
-
-	  	for (var j = 0; j < asCode.length; j++) {
-	    	decrypted.push(String.fromCharCode(asCode[j]));
+function rot13(str) { // LBH QVQ VG!
+  var asCode = [];
+  var decrypted = [];
+    for (var i = 0; i < str.length; i++) {
+	if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+	if (str.charCodeAt(i) - 13 < 65) {
+	  asCode.push(str.charCodeAt(i) + 26 - 13);
+	} else {
+	  asCode.push(str.charCodeAt(i) - 13);
 	}
-	  	return decrypted.join('');
+	} else {
+	  asCode.push(str.charCodeAt(i));
+	}
 	}
 
-	// Change the inputs below to test
-	rot13("SERR PBQR PNZC");
+   for (var j = 0; j < asCode.length; j++) {
+   	decrypted.push(String.fromCharCode(asCode[j]));
+	}
+	 return decrypted.join('');
+	}
+
+// Change the inputs below to test
+  rot13("SERR PBQR PNZC");
